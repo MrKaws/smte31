f604816e419dda9dcf60f5b300a711d11582e10e
Anh <anh1503@hotmail.com>
2014-02-19 19:45:42 +0100

    Week 1
    ToDoList

---
+++ Week 1/ToDoList/ToDoList/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  ToDoList
+//
+//  Created by Anh Tran on 17-02-14.
+//  Copyright (c) 2014 Anh Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Week 1/ToDoList/ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Anh Tran on 17-02-14.
+//  Copyright (c) 2014 Anh Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
