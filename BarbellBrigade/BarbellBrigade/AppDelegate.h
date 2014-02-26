a72f07a82621d493050e066b7ff2a8450a7715bd
Anh <anh1503@hotmail.com>
2014-02-26 22:38:04 +0100

    app

---
+++ BarbellBrigade/BarbellBrigade/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BarbellBrigade
+//
+//  Created by Anh Tran on 26-02-14.
+//  Copyright (c) 2014 Fontys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BarbellBrigade/BarbellBrigade/PageContentViewController.m
@@ -0,0 +1,40 @@
+//
+//  PageContentViewController.m
+//  BarbellBrigade
+//
+//  Created by Anh Tran on 26-02-14.
+//  Copyright (c) 2014 Fontys. All rights reserved.
+//
+
+#import "PageContentViewController.h"
+
+@interface PageContentViewController ()
+
+@end
+
+@implementation PageContentViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+    self.backgroundImageView.image = [UIImage imageNamed:self.imageFile];
+    self.titleLabel.text = self.titleText;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
